e45cb70159db493f2c20d22ca89e66ab62a4ff75
Adrian Moses <adrian@adrianmoses.com>
2022-04-13 22:33:08 -0400

    first commit

---
+++ HelloAppWatch/HelloAppWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  HelloAppWatch WatchKit Extension
+//
+//  Created by Adrian Moses on 4/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var sentence = EmojiSentence()
+    var body: some View {
+        VStack {
+            Image("Cookie")
+                .resizable()
+                .scaledToFit()
+                .overlay(
+                    Text(sentence.emoji)
+                        .font(.title3)
+                        .padding(.top, 10)
+                        .buttonStyle(.plain)
+                )
+            Text(sentence.text)
+                .font(.caption)
+                .padding(.top, 20)
+        }
+        .onTapGesture {
+            sentence.next()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloAppWatch/HelloAppWatch WatchKit Extension/HelloAppWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloAppWatchApp.swift
+//  HelloAppWatch WatchKit Extension
+//
+//  Created by Adrian Moses on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
